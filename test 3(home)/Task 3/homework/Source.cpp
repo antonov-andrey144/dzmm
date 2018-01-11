d156f2bea20776dff9a34b829e80832a59abfe87
antonov-andrey144 <antonov-andrey144@yandex.ru>
2018-01-11 15:26:05 +0300

    added test3

---
+++ test 3(home)/Task 3/homework/Source.cpp
@@ -0,0 +1,202 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+enum class Status
+{
+	start,
+	readNextSymbolStart,
+	digitWas,
+	alphaWas,
+	digit2Was,
+	groop,
+	groopWas,
+	dWas,
+	mWas,
+	fail
+};
+
+bool isRightAlpha(char alpha)
+{
+	if (alpha == 'B' || alpha == 'M' || alpha == 'S' || alpha == 'm')
+
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main(int argc, char** argv) {
+	char str[100];
+	printf("Enter the string you want to check: ");
+	scanf("%s", &str);
+	int i = 0;
+	Status state = Status::start;
+	while (str[i] != '\0')
+	{
+		switch (state)
+		{
+		case Status::start:
+		{
+			if (isdigit(str[i]))
+			{
+				state = Status::readNextSymbolStart;
+				++i;
+			}
+			else
+			{
+				state = Status::fail;
+			}
+			break;
+		}
+
+		case Status::digitWas:
+		{
+			if (isalpha(str[i]) && isRightAlpha(str[i]))
+			{
+				state = Status::alphaWas;
+				++i;
+			}
+			else
+			{
+				state = Status::fail;
+			}
+			break;
+		}
+
+		case Status::alphaWas:
+		{
+			if (isdigit(str[i]))
+			{
+				if (str[i] >='1')
+				{
+					state = Status::groop;
+					i++;
+				}
+				else 
+				{
+					state = Status::groopWas;
+				}
+			}
+
+			else
+			{
+				state = Status::fail;
+				i++;
+			}
+			break;
+		}
+
+		case Status::groop:
+		{
+			if (str[i] == '0')
+			{
+				state = Status::groopWas;
+				++i;
+			}
+			else if (str[i] == '-')
+			{
+				state = Status::groopWas;
+			}
+			else
+			{
+				state = Status::fail;
+			}
+			break;
+		}
+
+		case Status::groopWas:
+		{
+			if (str[i] == '-')
+			{
+				state = Status::dWas;
+			}
+			else
+			{
+				state = Status::fail;
+				i++;
+			}
+			break;
+		}
+
+		case Status::dWas:
+		{
+			if (str[i] == 'm')
+			{
+				state = Status::mWas;
+				i++;
+			}
+			else
+			{
+				state = Status::fail;
+				i++;
+			}
+			break;
+		}
+
+		case Status::mWas:
+		{
+			if (str[i] == 'm')
+			{
+				state = Status::mWas;
+				break;
+			}
+			else
+			{
+				state = Status::fail;
+			}
+			break;
+		}
+
+		case Status::readNextSymbolStart:
+		{
+
+			if (isdigit(str[i]))
+			{
+				state = Status::digitWas;
+				++i;
+			}
+
+			else 
+			{
+				state = Status::fail;
+			}
+			break;
+		}
+
+		case Status::fail:
+		{
+			break;
+		}
+
+		default:
+		{
+			state = Status::fail;
+			break;
+		}
+		}
+
+		if (state == Status::fail)
+		{
+			break;
+		}
+	}
+
+	if (state == Status::mWas)
+	{
+		printf("String is right");
+	}
+	else
+	{
+		printf("String is not right");
+	}
+
+	int pause = 0;
+	scanf("%d", &pause);
+	return 0;
+}
---
+++ test 3(home)/task 2.22/homework/Source.cpp
@@ -0,0 +1,158 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+struct Node
+{
+	int data = 0;
+	Node* next = nullptr;
+};
+
+struct CircleList
+{
+	Node* start = nullptr;
+	Node* end = nullptr;
+};
+
+Node* newNode(int data, Node *next = nullptr)
+{
+	Node* result = new Node();
+	result->data = data;
+	result->next = next;
+	return result;
+}
+
+void addElement(CircleList& list, int element)
+{
+	if (list.start == nullptr)
+	{
+		list.start = newNode(element);
+		list.start->next = list.start;
+		list.end = list.start;
+	}
+	else
+	{
+		list.end->next = newNode(element, list.end->next);
+		list.end = list.end->next;
+	}
+}
+
+void addElementToStart(CircleList &list, int element)
+{
+	if (list.start == nullptr)
+	{
+		list.start = newNode(element);
+		list.start->next = list.start;
+		list.end = list.start;
+	}
+	else
+	{
+		Node *node1 = newNode(element);
+		Node *node2 = list.start;
+		list.start = node1;
+		list.start->next = node2;
+		list.end->next = list.start;
+	}
+}
+
+void printList(CircleList& list)
+{
+	Node* node = list.start;
+
+	if (node == nullptr)
+	{
+		printf("EMPTY\n");
+	}
+	else
+	{
+		do
+		{
+			printf("%d ", node->data);
+			node = node->next;
+		} while (node != list.start);
+
+		printf("\n");
+	}
+}
+
+void deleteList(CircleList& list)
+{
+	list.end->next = nullptr;
+	while (list.start != nullptr)
+	{
+		Node* node = list.start;
+		list.start = list.start->next;
+		delete node;
+	}
+	list.end = nullptr;
+}
+
+void printMenu()
+{
+	printf("\tМеню\n");
+	printf("\t0 - Выход\n");
+	printf("\t1 - заполнить список случайными значениями (размер списка и диапазон значений задаются с консоли)\n");
+	printf("\t2 - развернуть список\n");
+	printf("\t3 - распечатать список\n");
+}
+
+void initList(CircleList &list, int len, int a, int b)
+{
+	for (int i = 0; i < len; ++i)
+	{
+		addElement(list, rand() % (b - a) + a);
+	}
+}
+
+CircleList reversList(CircleList &list, int len)
+{
+	CircleList result;
+	Node* tempNode = list.start;
+	for (int i = 0; i < len; ++i)
+	{
+		addElementToStart(result, tempNode->data);
+		tempNode = tempNode->next;
+	}
+	deleteList(list);
+	return result;
+}
+
+void makeChoice(int choice, CircleList &list, int &len)
+{
+	int a = 0;
+	int b = 0;
+	switch (choice)
+	{
+	case 1:
+		printf("Введите длину списка и диапазон значений от а до b");
+		scanf("%d %d %d", &len, &a, &b);
+		initList(list, len, a, b);
+		break;
+	case 2:
+		list = reversList(list, len);
+		break;
+	case 3:
+		printList(list);
+		break;
+	}
+}
+int main()
+{
+	CircleList list;
+	setlocale(LC_ALL, "Russian");
+	int len = 0;
+
+	int choice = 0;
+
+	do {
+		printMenu();
+		printf("Ваш выбор: ");
+		scanf("%d", &choice);
+		makeChoice(choice, list, len);
+	} while (choice != 0);
+
+	deleteList(list);
+
+	int pause = 0;
+	scanf("%d", &pause);
+	return 0;
+}
